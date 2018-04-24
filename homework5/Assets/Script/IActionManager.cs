cd6f2ae6a211f43975ba003a5aca2779e599b54e
liangyy75 <3249475320@qq.com>
2018-04-24 21:35:31 +0800

    the fifth commit

---
+++ homework5/Assets/Script/IActionManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IActionManager {
+    int getDiskNumber();
+    void setDiskNumber(int num);
+    void startThrow(Queue<GameObject> diskQueue);
+}
---
+++ homework5/Assets/Script/IUserAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { ROUND_START, ROUND_FINISH, RUNNING, PAUSE, START, FUNISH}
+public enum ActionMode { NOTSET, PHYSIC, KINEMATIC }
+
+public interface IUserAction{
+    ActionMode getActionMode();
+    void setActionMode(ActionMode mode);
+    GameState getGameState();
+    void setGameState(GameState gameState);
+    int getScore();
+    void hit(Vector3 pos);
+}
